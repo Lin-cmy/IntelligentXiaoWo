0949384fd102c074f8c890556c14fab6b1a863e4
Lin-cmy <1127934124@qq.com>
2025-06-28 12:03:56 +0800

    更新home页，实现了前端和后端数据的交互

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,36 +1,56 @@
 Page({
   data: {
-    selectedRoom: '全部',
+    homeId: null,
+    homeName: '',
+    newFamilyName: '',
+    newFamilyAddress: '',
+    homes: [],
+    rooms: [],
+    devices: [],
+    filteredDevices: [],
+    selectedRoomId: '0',
     showPopup: false,
-    devices: [
-      { id: 1, name: '卧室灯', room: '卧室', icon: '/images/卧室灯.jpg' },
-      { id: 2, name: '客厅空调', room: '客厅', icon: '/images/空调.jpg' },
-      { id: 3, name: '冰箱', room: '厨房', icon: '/images/冰箱.jpg' },
-      { id: 4, name: '空气净化器', room: '书房', icon: '/images/空气净化器.jpg' }
-    ],
-    filteredDevices: []
+    showCreatePopup: false
   },
+  
+  // 初始化界面，查询用户所在的家庭，默认展示第一个家庭
   onLoad() {
-    this.filterDevices();
+    this.homeget();
   },
+
   onFilterTap(e) {
+    console.log('dataset:', e.currentTarget.dataset);
     this.setData({
-      selectedRoom: e.currentTarget.dataset.room
-    }, this.filterDevices);
-  },
-  filterDevices() {
-    const { devices, selectedRoom } = this.data;
-    this.setData({
-      filteredDevices: selectedRoom === '全部'
-        ? devices
-        : devices.filter(item => item.room === selectedRoom)
+      selectedRoomId: String(e.currentTarget.dataset.roomid)
+    }, () => {
+      this.filterDevices();
     });
   },
+
+  filterDevices() {
+    const { devices, selectedRoomId } = this.data;
+    this.setData({
+      filteredDevices: selectedRoomId === '0'
+        ? devices
+        : devices.filter(item => String(item.roomId) === selectedRoomId)
+    });
+  },
+
+  // 开关悬浮窗
   onTitleTap() {
     this.setData({
       showPopup: !this.data.showPopup
     });
   },
+
+  // 选择当前家庭
+  onFamilyChangeTap(e) {
+    const homeId = Number(e.currentTarget.dataset.id);
+    const homeName = e.currentTarget.dataset.name;
+    this.setData({ homeId: homeId, homeName: homeName, showPopup: false }, () => this.homeview());
+  },
+
+  // 跳转到家庭管理界面
   onFamilyManagerTap() {
     wx.navigateTo({
       url: '/pages/familymanager/familymanager',
@@ -38,5 +58,150 @@ Page({
         this.setData({ showPopup: false });
       }
     });
+  },
+
+  // 创建家庭
+  onFamilyCreateTap() {
+    this.setData({ showCreatePopup: true });
+  },
+  onFamilyNameInput(e) {
+    this.setData({ newFamilyName: e.detail.value });
+  },
+  onFamilyAddressInput(e) {
+    this.setData ({ newFamilyAddress: e.detail.value });
+  },
+  onConfirmCreate() {
+    if (!this.data.newFamilyName.trim()) {
+      wx.showToast({ title: '请输入家庭名称', icon: 'none' });
+      return;
+    }
+    if (!this.data.newFamilyAddress.trim()) {
+      wx.showToast({ title: '请输入家庭地址', icon: 'none' });
+      return;
+    }
+    this.homecreate(() => {
+      this.homeget(() => {
+        this.setData({ showCreatePopup: false, newFamilyName: '', newFamilyAddress: '' });
+      });
+    });
+  },
+  onPopupClose() {
+    this.setData({ showCreatePopup: false, newFamilyName: '', newFamilyAddress: '' });
+  },
+
+  // /home/get
+  homeget(callback) {
+    wx.request ({
+      url: 'http://localhost:8080/home/get',
+      method: 'GET',
+      header: { 'Authorization': 'Bearer ' + wx.getStorageSync('token') },
+      success: (res) => {
+        if (res.statusCode === 200 ) {
+          // wx.showToast({ title: '查询成功' })
+          const homes = res.data.homes.map(item => ({
+            id: item.id,
+            name: item.name
+          }));
+          this.setData({ homes: homes }, () => {
+            if (homes.length > 0) {
+              this.setData({ homeId: homes[0].id, homeName: homes[0].name }, () => {
+                this.homeview();
+                this.filterDevices();
+                if (typeof callback === 'function') callback();
+              });
+            } else {
+              if (typeof callback === 'function') callback();
+            }
+          });
+        } else {
+          wx.showToast({
+            title: res.data.message || '查询失败',
+            icon: 'none'
+          });
+          if (typeof callback === 'function') callback();
+        }
+      },
+      fail: () => {
+        wx.showToast({
+          title: '网络错误',
+          icon: 'none'
+        });
+        if (typeof callback === 'function') callback();
+      }
+    });
+  },
+
+  // /home/view
+  homeview() {
+    const homeId = this.data.homeId;
+    wx.request({
+      url: 'http://localhost:8080/home/view?homeId=' + homeId,
+      method: 'GET',
+      header: { 
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + wx.getStorageSync('token')
+      },
+      success: (res) => {
+        if (res.statusCode === 200) {
+          // wx.showToast({ title: 'view成功' });
+          const rooms = res.data.rooms.map(item => ({
+            id: item.id,
+            name: item.name
+          }));
+          const devices = res.data.devices.map(item => ({
+            id: item.id,
+            name: item.name,
+            roomId: String(item.roomId)
+          }));
+          this.setData({
+            rooms: rooms,
+            devices: devices,
+            selectedRoomId: '0'
+          }, this.filterDevices);
+        } else {
+          wx.showToast({
+            title: res.data.message || 'view失败',
+            icon: 'none'
+          });
+        }
+      },
+      fail: () => {
+        wx.showToast({
+          title: '网络错误',
+        });
+      }
+    })
+  },
+
+  // /home/create
+  homecreate(callback) {
+    wx.request({
+      url: 'http://localhost:8080/home/create',
+      method: 'POST',
+      header: { 'Authorization': 'Bearer ' + wx.getStorageSync('token')},
+      data: {
+        "name": this.data.newFamilyName,
+        "address": this.data.newFamilyAddress
+      },
+      success: (res) => {
+        if (res.statusCode === 201) {
+          wx.showToast({
+            title: '创建成功',
+            icon: 'none'
+          });
+          if (typeof callback === 'function') callback();
+        } else {
+          wx.showToast({
+            title: res.data.message || '创建失败',
+            icon: 'none'
+          });
+        }
+      },
+      fail: () => {
+        wx.showToast({
+          title: '网络错误',
+        });
+      }
+    })
   }
 });
--- pages/login/login.js
+++ pages/login/login.js
@@ -66,57 +66,7 @@ Page({
         this.setData({ isLoading: false });
       }
     })
-
-    // try {
-    //   const res = await this.requestLogin();
-      
-    //   if (res.data.code === 200) {
-    //     // 保存用户信息
-    //     wx.setStorageSync('token', res.data.data.token);
-    //     wx.setStorageSync('userId', res.data.data.userId);
-    //     wx.setStorageSync('userInfo', res.data.data.userInfo);
-
-    //     wx.showToast({
-    //       title: '登录成功',
-    //       icon: 'success'
-    //     });
-
-    //     setTimeout(() => {
-    //       this.redirectToMain();
-    //     }, 1500);
-    //   } else {
-    //     wx.showToast({
-    //       title: res.data.message || '登录失败',
-    //       icon: 'none'
-    //     });
-    //   }
-    // } catch (error) {
-    //   wx.showToast({
-    //     title: '网络错误，请重试',
-    //     icon: 'none'
-    //   });
-    // } finally {
-    //   this.setData({ isLoading: false });
-    // }
   },
-
-  // requestLogin() {
-  //   return new Promise((resolve, reject) => {
-  //     wx.request({
-  //       url: 'YOUR_API_BASE_URL/auth/login', // 替换为实际API地址
-  //       method: 'POST',
-  //       header: {
-  //         'Content-Type': 'application/json'
-  //       },
-  //       data: {
-  //         phone: this.data.phone,
-  //         password: this.data.password
-  //       },
-  //       success: resolve,
-  //       fail: reject
-  //     });
-  //   });
-  // },
 
   redirectToMain() {
     wx.switchTab({
